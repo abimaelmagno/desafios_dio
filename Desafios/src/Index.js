5e03575cd8eeba54c458c8f1d97851adacb7fb5b
abimaelmagno <abimaelmagno@hotmail.com>
2024-06-11 21:14:32 -0300

    operadores

---
+++ Desafios/src/Index.js
@@ -0,0 +1,8 @@
+console.log ("Hi people")
+let primeiroNumero = 124
+let segundoNumero = 34
+console.log (primeiroNumero - segundoNumero)
+console.log ("Abimael Magno " + "do Ouro Filho")
+let precoProduto = 100.45
+let valorComTaxa = precoProduto * 4
+console.log (valorComTaxa)
