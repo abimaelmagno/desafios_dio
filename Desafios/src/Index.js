2d94f21d18719b3e05c4c01d9d74b9ccfc849770
abimaelmagno <abimaelmagno@hotmail.com>
2024-06-12 10:51:30 -0300

    operadoresmatematicos 2

--- Desafios/src/Index.js
+++ Desafios/src/Index.js
@@ -8,7 +8,22 @@ let valorComTaxa = precoProduto * 4
 console.log ("Resultado do produto com taxa é " + valorComTaxa)
 let pessoasDaCompra = 5
 let valorPorPessoa = valorComTaxa / pessoasDaCompra
-console.log ("Valor que ficou por pessoa " + valorPorPessoa)
+console.log ("Entre 4 amigos, o valor que ficou por pessoa " + valorPorPessoa)
 //se fosse modulo
 let operacaoModulo = valorComTaxa % pessoasDaCompra
 console.log ("Valor que resta depois de dividir por partes iguais " + operacaoModulo)
+//operadores de incremento e decremento
+//incremento
+operacaoModulo ++
+console.log ("Novo valor com incremento " + operacaoModulo)
+//decremento
+operacaoModulo --
+console.log ("Novo valor com decremento " + operacaoModulo)
+//Operadores de atribuição
+operacaoModulo += 0.2
+console.log ("Novo valor com operadores de atribuição " + operacaoModulo)
+operacaoModulo -= 0.0000000000000115
+console.log ("Novo valor com operadores de atribuição " + operacaoModulo)
+//Expressões aritmeticas
+operacaoModulo = (((operacaoModulo * 2) + 10 ) - 4) / 2
+console.log ("Novo valor com expressões artimeticas " + operacaoModulo)
