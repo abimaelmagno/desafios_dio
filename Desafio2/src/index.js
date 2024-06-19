21044897ca718f1cb43e2fb36dc6f6c4612c8f04
abimaelmagno <abimaelmagno@hotmail.com>
2024-06-18 21:18:45 -0300

    SegundoDesafioCompleto

--- Desafio2/src/index.js
+++ Desafio2/src/index.js
@@ -1,1 +1,15 @@
-console.log("teste")
+function calcularNivelRanqueadas(vitorias, derrotas) {
+    const saldoVitorias = vitorias - derrotas;
+    const nivel = 
+        vitorias < 10 ? "Ferro" :
+        vitorias <= 20 ? "Bronze" :
+        vitorias <= 50 ? "Prata" :
+        vitorias <= 80 ? "Ouro" :
+        vitorias <= 90 ? "Diamante" :
+        vitorias <= 100 ? "Lendário" : "Imortal";
+    return `O Herói tem saldo de ${saldoVitorias} está no nível de ${nivel}`;
+}
+
+// Exemplos de uso da função
+console.log(calcularNivelRanqueadas(150, 2)); 
+
